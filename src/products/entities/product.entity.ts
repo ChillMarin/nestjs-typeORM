54fd93a18ef7b35ab1ef8effee49bc9ed29dde53
Marcel Marin <marcelmarin256@gmail.com>
2023-01-22 17:44:42 -0400

    Modificand una entidad

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 // Decorador que indica que es una entidad de la base de datos
 @Entity()
@@ -8,19 +14,36 @@ export class Product {
   id: number;
 
   //Columna de tipo varchar, no nula, con longitud de 100 y que su valor sea unico, es decir no pueden haber 2 con el mismo nombre
-  @Column({type: 'varchar', length: 255, nullable: false, unique: true})
+  @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
   name: string;
 
   // Columna tipo varchar, sin limite de texto, no nula
-  @Column({type: 'text', nullable: false})
+  @Column({ type: 'text', nullable: false })
   description: string;
 
-  @Column({type:'int'})
+  @Column({ type: 'int' })
   price: number;
 
-  @Column({type:'int'})
+  @Column({ type: 'int' })
   stock: number;
 
-  @Column({type:'varchar'})
+  @Column({ type: 'varchar' })
   image: string;
+
+  // Columna que se va a llenar automaticamente con la fecha de creacion
+  // e; timestampz es para que guarde la hora en UTC y no en la hora local, para que no haga eso le quitamos la tz
+  @CreateDateColumn({
+    type: 'timestamptz',
+    // el default es para que se llene automaticamente con la fecha actual
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  // Columna que se va a llenar automaticamente con la fecha de actualizacion
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    // el default es para que se llene automaticamente con la fecha actual
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
 }
