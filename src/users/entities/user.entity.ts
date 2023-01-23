39665ed91e46462919c699125c7f2082e4c4286f
Marcel Marin <marcelmarin256@gmail.com>
2023-01-23 11:29:36 -0400

    Relaciones uno a uno

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,48 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  //usados para las relaciones 1 a 1
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import { Customer } from './customer.entity';
+
+// Decorador que indica que es una entidad de la base de datos
+@Entity()
 export class User {
+  @PrimaryColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   email: string;
-  password: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string; // encript, deberia ser encriptado
+
+  @Column({ type: 'varchar', length: 100 })
   role: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
+  // Relacion 1 a 1 con la entidad customer
+  // nullable: true porque no todos los usuarios son clientes
+  // JoinColumn es para que se cree una columna en la tabla de customer que se llame user_id y que sea la primary key de la tabla user
+  // es decir que se cree una relacion entre las tablas user y customer
+  @OneToOne(() => Customer, (customer) => customer.user, { nullable: true })
+  @JoinColumn()
+  customer: Customer;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersService } from './services/users/users.service';
 import { CustomersService } from './services/customers/customers.service';
 
 import { ProductsModule } from '../products/products.module';
 import { UsersController } from './controllers/users.controller';
 
+import { Customer } from './entities/customer.entity';
+import { User } from './entities/user.entity';
+
 @Module({
   controllers: [UsersController],
   providers: [UsersService, CustomersService],
-  imports: [ProductsModule],
+  // typeorm module for feature, para que sepa que entidades va a usar de products y que las va a importar de la carpeta entities de products
+  imports: [ProductsModule, TypeOrmModule.forFeature([User, Customer])],
 })
 export class UsersModule {}
