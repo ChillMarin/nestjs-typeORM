fa17d2a182a86bfd336a9fd0bc8a965d8a115bc2
Marcel Marin <marcelmarin256@gmail.com>
2023-01-24 15:49:18 -0400

    Resolviendo muchos a muchos

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,4 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToMany,
+} from 'typeorm';
+
+import { Product } from './product.entity';
+
+@Entity()
 export class Category {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
+  // Relacion de muchos a muchos, es decir una categoria tiene muchos productos y un producto tiene muchas categorias
+  // El () => Product es para que sepa que es una entidad de la base de datos
+  // El (product) => product.categories es para que sepa que es una relacion de muchos a muchos, es decir una categoria tiene muchos productos y un producto tiene muchas categorias
+  @ManyToMany(() => Product, (product) => product.categories)
+  products: Product[];
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -5,8 +5,11 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 import { Brand } from './brand.entity';
+import { Category } from './category.entity';
 
 // Decorador que indica que es una entidad de la base de datos
 @Entity()
@@ -55,4 +58,14 @@ export class Product {
   // No es necesario poner el JoinColumn automaticamente sabe que el que tiene el decorador ManyToOne es el que tiene la llave foranea, en el caso pasado de 1a1 si era necesario poner el JoinColumn
   @ManyToOne(() => Brand, (brand) => brand.products)
   brand: Brand;
+
+  // Relacion de muchos a muchos, es decir un producto puede pertenecer a muchas categorias y una categoria puede tener muchos productos
+  // El () => Category es para que sepa que es una entidad de la base de datos
+  // El (category) => category.products es para que sepa que es una relacion de muchos a muchos, es decir un producto puede pertenecer a muchas categorias y una categoria puede tener muchos productos, y esto hace que sea una relacion bidireccional y guarde las relaciones en la ttabla product_category
+  // No es necesario poner el JoinColumn automaticamente sabe que el que tiene el decorador ManyToMany es el que tiene la llave foranea, en el caso pasado de 1a1 si era necesario poner el JoinColumn
+  @ManyToMany(() => Category, (category) => category.products)
+  // El JoinTable es para que sepa que la tabla que va a guardar las relaciones es product_category y solo debe ir en el ManyToMany que tiene la llave foranea es decir puedo elegir cual debe de tenerlo segun sea el caso y esto crea la tabla product_cagories_category
+  @JoinTable()
+  categories: Category[];
+
 }
