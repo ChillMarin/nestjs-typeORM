4fe5686d3a21b923b1f1e4afb3bb46d51a566aa0
Marcel Marin <marcelmarin256@gmail.com>
2023-01-26 20:20:19 -0400

    Serializar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -18,6 +18,8 @@ async function bootstrap() {
       },
     }),
   );
+  // Admitimos serialiacion
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
     .setTitle('API')
--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude, Expose } from 'class-transformer';
 import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
@@ -29,6 +30,36 @@ export class Order {
   @ManyToOne(() => Customer, (customer) => customer.orders)
   customer: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderItem, (item) => item.order)
   items: OrderItem[];
+
+  @Expose()
+  get products() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+          // agregamos el id del item para poder eliminarlo si lo deseamos
+          itemId: item.id,
+        }));
+    }
+    return [];
+  }
+
+  @Expose()
+  get total() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        // reduce es un metodo de array que recibe una funcion y un valor inicial donde tenemos el total de la suma de los items y es un iterador del array donde total se va acumulando por cada item
+        .reduce((total, item) => {
+          const totalItem = item.product.price * item.quantity;
+          return total + totalItem;
+        }, 0);
+    }
+    return 0;
+  }
 }
